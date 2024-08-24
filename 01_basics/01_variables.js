8fda918035113074bfb440c86d446d53c208ba61
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-08-24 20:42:01 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = 223533
+let accountEmail = "nisha@gmai.com"
+var accountPassword = "123456"
+accountCity = "Almora"
+
+//accountId = 1
+accountEmail = "mehta1@gmail.com"
+accountPassword = "222222"
+accountCity = "dehradun"
+
+console.table([accountId, accountEmail, accountPassword, accountPassword]);
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // all js code as newer version
+
+//alert(2+2) // we are using nodejs, not browser
+
+console.log(2+2)
+
+console.log("Nisha")
+
+let name = "Nisha"
+let age = 20
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//null => standalone value
+//undefined
+//symbol => unique
+
+// object
+
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
