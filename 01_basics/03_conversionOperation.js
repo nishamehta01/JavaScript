61fbcc277f38f8bbb47c8041a91f801c274bed7a
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-08-27 18:06:22 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,49 @@
+let score = true
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// "24" => 24
+// "23abc" => NaN(not an number)
+// true = 1 
+// false => 0
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Nisha" => true
+
+let  n = 21
+
+let stringNumber = String(n)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+// ------------Operations-------
+
+
+let value = 9
+let negValue = -value
+console.log(negValue)
+
+let str1 = "Good"
+let str2 = " Morning!"
+let str3 = str1+str2;
+//console.log(str3);
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
+
+
