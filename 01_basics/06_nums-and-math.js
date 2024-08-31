987311fec73c078e4da208604d7f9bc876542d0a
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-08-31 02:42:09 +0000

    nums-and-math

---
+++ 01_basics/06_nums-and-math.js
@@ -0,0 +1,44 @@
+const score = 200
+console.log(score);
+
+
+const balance = new Number(100)
+console.log(balance);
+console.log(balance.toFixed(1));
+
+const otherNumber = 123.8976
+
+console.log(otherNumber.toPrecision(4));
+
+const hundreds = 1000000
+console.log(hundreds.toLocaleString('en-In'));
+
+//--------------------MATHS----------------//
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(7.2));
+console.log(Math.ceil(6.2));
+console.log(Math.floor(5.9));
+console.log(Math.max(6,23,45,5,3));
+
+
+console.log(Math.random()); 
+console.log((Math.random()*10) +1);
+console.log(Math.floor(Math.random()*10) +1);
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+
+
+
+
+
+
+
+
+
+
