071a10f0710e3856941430e880053c303763d3dc
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-09-07 06:27:29 +0000

    functions

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -32,3 +32,33 @@ console.log(loginUserMsg())
 
 
 
+function calculateCartPrice(val1,val2,...num1){
+ return num1
+}
+console.log(calculateCartPrice(200,300,400))
+
+
+const user = {
+    username: "Nisha",
+    price: 199
+}
+function handleObject(anyObject){
+   console.log(`Username is ${anyObject.username} and price is ${anyObject.price}`);
+   
+}
+
+//handleObject(user)
+handleObject({      // we can direct pass object
+    username: "Nisha",
+    price: 199
+})
+
+
+const myNewArray = [100,200,400,600,500]
+
+function returnSecondValue(getArray){
+   return getArray[3]
+}
+//console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([100,300,700,900,450]));
+
