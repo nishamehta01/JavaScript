0ef025895e42d46c433bb40610cd9258d8c3d7be
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-09-09 18:14:29 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,10 +4,53 @@ const user = {
 
     welcomMessage: function() {
         console.log(`${this.username} , welcome to website`);
+        //console.log(this);
+        
         
     }
 }
 
-user.welcomMessage()
-user.username = "nish"
-user.welcomMessage()
+//user.welcomMessage()
+//user.username = "nish"
+//user.welcomMessage()
+
+//console.log(this);
+
+
+//function monster(){
+// let username = "nisha"
+  //  console.log(this.username);
+    
+//}
+//monster()
+
+
+//const monster = function(){
+  //  let username = "nisha"
+    // console.log(this.username)
+//}
+//monster()
+
+
+const monster = () => {
+    let username = "nisha"
+     console.log(this)
+}
+//monster()
+
+//const addTwo = (num1, num2) => {
+  //  return num1 + num2 
+//}
+
+//console.log(addTwo(3,4));
+
+
+
+//const addTwo = (num1, num2) => num1 + num2 
+const addTwo = (num1, num2) => (num1 + num2)
+const add = (num1, num2) => ({username: "nisha"})
+
+console.log(addTwo(3,4))
+console.log(add(1,2));
+
+
