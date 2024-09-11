c757fce91c1ac8cd72c793b6972da604c7af22e9
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-09-11 10:17:59 +0000

    control_flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,55 @@
+// If Statement
+
+
+//const temprature = 41
+//if(temprature == 40){
+    //console.log("less than 50");
+    
+//}else{
+    //console.log("more than 50");
+    
+//}
+//console.log("Executed");
+// <, >, <=, >=, ==, !=, ===, !==
+
+
+const score = 200 
+if(score>100){
+    const power = "fly"
+    //console.log(`User power: $(power)`);
+    
+}
+//console.log(`User power: $(power)`); 
+
+
+const balance = 1000
+//if (balance>500) console.log("test") ------ this is not good approach to compile
+
+if(balance<500) {
+    //console.log("less than 500");
+    
+}else if(balance<750){
+    //console.log("less than 750");
+    
+}else if(balance<900){
+    //console.log("less than 750");
+    
+}else{
+   // console.log("less than 1200");
+    
+}
+
+const userLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedFromEmail = true
+
+if(userLoggedIn && debitCard){
+    console.log("Alllow to buy course");
+    
+}
+if(loggedInFromGoogle || loggedFromEmail){
+    console.log("User logged in");
+    
+}
+
