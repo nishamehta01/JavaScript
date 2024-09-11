895604289cb4649d9c568c5c2b1a364fa8f6bb3e
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-09-11 15:51:41 +0000

    control_flow

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -6,3 +6,33 @@ if(userEmail){
     console.log("Don't have user Email");
     
 } 
+
+//--------------falsy values----
+//false, 0, -0, BigInt(0n), "", null, undefined, Nan(not an number)
+
+
+//----------------truty values-----
+// "0", 'false', " ", []. {}, function(){}
+
+if(userEmail.length ===0){
+    console.log("Array is empty");
+    
+}
+ 
+//---- Nullish Coalescing Operator (??): null undefined
+
+let val1;
+//val1 =5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 12
+val1 = null ?? 10 ?? 21
+
+console.log((val1));
+
+
+//----Terninay operator ----
+//condition ? true: false
+
+const iceCream = 100
+iceCream <= 80 ? console.log("less than 80") : console.log("more than 80");
+
