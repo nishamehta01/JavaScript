ea38e01398ddba76469c1bd816b2b1bf962afb50
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-09-21 19:33:07 +0000

    iteration

---
+++ 05_iteration/five.js
@@ -0,0 +1,12 @@
+const coading = ["js", "ruby", "java", "python", "cpp"]
+
+//coading.forEach( function (val) {
+ // console.log(val);
+  
+//})
+
+coading.forEach( (item) => {
+  //console.log(item);
+  
+})
+
---
+++ 05_iteration/four.js
@@ -0,0 +1,29 @@
+const myObject = {
+    js: 'javaScript',
+    cpp: 'c++',
+    rb: "ruby",
+    swift: "swift by apple"
+}
+
+for (const key in myObject) {
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
+    
+}
+
+const programming = ["js", "rb", "py", "java", "cpp"]
+for (const key in programming) {
+    console.log(key);
+    
+}
+
+// const map = new Map()
+// map.set(`IN`, "India")
+// map.set(`USA`, "united states of america")
+// map.set(`Fr`, "france")
+// map.set(`IN`, "India")
+
+// for (const key in map) {
+//     console.log(map);
+    
+// }
+
