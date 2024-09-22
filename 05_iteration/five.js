5742515b50c183ddb7841f1fcdf6f343ccd5415e
Nisha Mehta <159520777+nishamehta01@users.noreply.github.com>
2024-09-22 01:36:52 +0000

    iteration

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -10,3 +10,35 @@ coading.forEach( (item) => {
   
 })
 
+// function printMe(item){
+//   console.log(item);
+  
+// }
+// coading.forEach(printMe)
+
+
+// coading.forEach( (item, index, arr) => {
+//  console.log(item, index, arr);
+ 
+// })
+
+const myCoading = [
+  {
+    languageName: "javascipt",
+    languageFileName: "java"
+  },
+  {
+    languageName: "java",
+    languageFileName: "js"
+  },
+  {
+    languageName: "Python",
+    languageFileName: "py"
+  }
+]
+
+myCoading.forEach( (item) => {
+
+  console.log(item.languageName);
+  
+})
